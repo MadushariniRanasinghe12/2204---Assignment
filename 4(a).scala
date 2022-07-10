1635e19c58d537e6b6a965c8a9173a88fbe85ef1
MadushariniRanasinghe12 <87421056+MadushariniRanasinghe12@users.noreply.github.com>
2022-07-10 17:37:37 +0530

    Update 4(a).scala

--- 4(a).scala
+++ 4(a).scala
@@ -5,9 +5,6 @@ def main(args:Array[String])={
     def tax(income:Int):Int=(income*12)/100;
     def takehomesalary(x1:Int,x2:Int):Double=income(x1,x2) - tax(income(x1,x2));
 
-    println(normal(40));
-    println(income(normal(40),ot(30)));
-    println(tax(income(normal(40),ot(30))));
-    println(takehomesalary(income(normal(40),ot(30)),tax(income(normal(40),ot(30)))));
+    println(takehomesalary(40,30));
     
 }
