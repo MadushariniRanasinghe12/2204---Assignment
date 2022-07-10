6a70b3863816e828cc05c1d7a48d0e62ce879a81
MadushariniRanasinghe12 <87421056+MadushariniRanasinghe12@users.noreply.github.com>
2022-07-10 18:15:05 +0530

    Update 4(b).scala

--- 4(b).scala
+++ 4(b).scala
@@ -1,7 +1,7 @@
 def main(args:Array[String])={
     def attendees(price:Int):Int=120 + (15-price)/5*20;
     def revenue(price:Int):Int=attendees(price)*price;
-    def cost(price:Int):Int=500 + attendees(price);
+    def cost(price:Int):Int=500 + 3 * attendees(price);
     def profit(price:Int):Int=revenue(price)-cost(price);
 
     println(profit(5));
